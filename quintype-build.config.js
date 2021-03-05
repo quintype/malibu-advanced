4d4f9c9e9d0e93f4a76a9a2d9f88febf177e53ec
nandap4790 <nandap4790@gmail.com>
2021-03-05 13:05:09 +0530

    fix: update build config and node build package

--- quintype-build.config.js
+++ quintype-build.config.js
@@ -1,1 +1,19 @@
-module.exports = require("@quintype/build/config/quintype-build");
+// module.exports = require("@quintype/build/config/quintype-build");
+
+module.exports = {
+  modifyWebpackConfig: {
+    includeLoadableConfig: {
+      loadableConfig: true,
+      entryFiles: {
+        topbarCriticalCss: "./app/isomorphic/components/header",
+        navbarCriticalCss: "./app/isomorphic/components/header/nav-bar"
+      }
+    }
+  },
+  modifyBabelConfig: {
+    includeLoadableConfig: {
+      loadableConfig: true,
+      loadableBabelPlugin: "@loadable/babel-plugin"
+    }
+  }
+};
