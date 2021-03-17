c19ed9c2f1405c70ef93c721d66b17d9450953ff
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-03-17 13:09:57 +0530

    feat: implement dummy dfp slot using native code

--- app/isomorphic/components/pages/home.js
+++ app/isomorphic/components/pages/home.js
@@ -1,15 +1,39 @@
 import { LazyCollection, LazyLoadImages, replaceAllStoriesInCollection, WithPreview } from "@quintype/components";
 import { object, shape } from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 import { getCollectionTemplate } from "../get-collection-template";
 
-export const HomePage = props => (
-  <div className="container">
-    <LazyLoadImages>
-      <LazyCollection collection={props.data.collection} collectionTemplates={getCollectionTemplate} lazyAfter={2} />
-    </LazyLoadImages>
-  </div>
-);
+const useDfpSlot = ({ path, size, id }) => {
+  useEffect(() => {
+    const googletag = window.googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    googletag.cmd.push(function() {
+      googletag.defineSlot(path, size, id).addService(googletag.pubads());
+      googletag.pubads().enableSingleRequest();
+      googletag.enableServices();
+    });
+    googletag.cmd.push(function() {
+      googletag.display(id);
+    });
+  }, [path, size, id]);
+};
+
+export const HomePage = props => {
+  useDfpSlot({
+    path: "/6355419/Travel/Europe/France/Paris",
+    size: [300, 250],
+    id: "banner-ad"
+  });
+
+  return (
+    <div className="container">
+      <LazyLoadImages>
+        <div id="banner-ad" style={{ width: "300px", height: "250px" }} />
+        <LazyCollection collection={props.data.collection} collectionTemplates={getCollectionTemplate} lazyAfter={2} />
+      </LazyLoadImages>
+    </div>
+  );
+};
 
 HomePage.propTypes = {
   data: shape({
