dfa1ae03af2b6ef114394a4d71177f2b7cee0a3c
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-03-18 15:24:08 +0530

    add another adslot at bottom

--- app/isomorphic/components/pages/home.js
+++ app/isomorphic/components/pages/home.js
@@ -4,56 +4,64 @@ import React, { useEffect } from "react";
 import { getCollectionTemplate } from "../get-collection-template";
 
 const useDfpSlot = ({ path, size, id }) => {
-  useEffect(() => {
-    const googletag = window.googletag || {};
-    googletag.cmd = googletag.cmd || [];
-    googletag.cmd.push(function() {
-      googletag
-        .defineSlot(path, size, id)
-        .setTargeting("test", "lazyload")
-        .addService(googletag.pubads());
+  const googletag = window.googletag || {};
+  googletag.cmd = googletag.cmd || [];
+  googletag.cmd.push(function() {
+    googletag
+      .defineSlot(path, size, id)
+      .setTargeting("test", "lazyload")
+      .addService(googletag.pubads());
 
-      googletag.pubads().enableLazyLoad({
-        fetchMarginPercent: 500,
-        renderMarginPercent: 200,
-        mobileScaling: 2.0
-      });
-
-      googletag.pubads().addEventListener("slotRequested", function(event) {
-        updateSlotStatus(event.slot.getSlotElementId(), "fetched");
-      });
-
-      googletag.pubads().addEventListener("slotOnload", function(event) {
-        updateSlotStatus(event.slot.getSlotElementId(), "rendered");
-      });
-      googletag.enableServices();
+    googletag.pubads().enableLazyLoad({
+      fetchMarginPercent: 0,
+      renderMarginPercent: 0,
+      mobileScaling: 0
     });
 
-    googletag.cmd.push(function() {
-      googletag.display(id);
+    googletag.pubads().addEventListener("slotRequested", function(event) {
+      updateSlotStatus(event.slot.getSlotElementId(), "fetched");
     });
 
-    function updateSlotStatus(slotId, state) {
-      var elem = document.getElementById(slotId + "-" + state);
-      elem.className = "activated";
-      elem.innerText = "Yes";
-    }
-  }, [path, size, id]);
+    googletag.pubads().addEventListener("slotOnload", function(event) {
+      updateSlotStatus(event.slot.getSlotElementId(), "rendered");
+    });
+
+    googletag.enableServices();
+  });
+
+  googletag.cmd.push(function() {
+    googletag.display(id);
+  });
+
+  function updateSlotStatus(slotId, state) {
+    var elem = document.getElementById(slotId + "-" + state);
+    elem.className = "activated";
+    elem.innerText = "Yes";
+  }
 };
 
 export const HomePage = props => {
-  useDfpSlot({
-    path: "/6355419/Travel/Europe/France/Paris",
-    size: [300, 250],
-    id: "banner-ad"
-  });
+  useEffect(() => {
+    useDfpSlot({
+      path: "/6355419/Travel/Europe/France/Paris",
+      size: [300, 250],
+      id: "banner-ad"
+    });
+
+    useDfpSlot({
+      path: "/6355419/Travel/Europe/France/Paris",
+      size: [300, 250],
+      id: "banner-ad-1"
+    });
+  }, []);
 
   return (
     <div className="container">
       <LazyLoadImages>
-        <div id="banner-ad" style={{ width: "300px", height: "250px" }} />
+        <div id="banner-ad" style={{ width: "300px", height: "250px" }}></div>
         <LazyCollection collection={props.data.collection} collectionTemplates={getCollectionTemplate} lazyAfter={2} />
       </LazyLoadImages>
+      <div id="banner-ad-1" style={{ width: "300px", height: "250px" }} />
     </div>
   );
 };
