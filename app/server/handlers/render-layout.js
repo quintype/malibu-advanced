583455365323c5be1b68942135e5376260a5f7a4
nandap4790 <nandap4790@gmail.com>
2021-04-01 10:59:49 +0530

    wip-1-get onesignal in the app

--- app/server/handlers/render-layout.js
+++ app/server/handlers/render-layout.js
@@ -26,7 +26,9 @@ const getConfig = state => {
     gaId: get(state, ["qt", "config", "publisher-attributes", "google_analytics", "id"], ""),
     isGaEnable: get(state, ["qt", "config", "publisher-attributes", "google_analytics", "is_enable"], false),
     cdnImage: get(state, ["qt", "config", "cdn-image"], ""),
-    isOnesignalEnable: get(state, ["qt", "config", "publisher-attributes", "onesignal", "is_enable"], false)
+    oneSignalSafariId: get(state, ["qt", "config", "publisher-attributes", "onesignal", "safari_web_id"], null),
+    isOnesignalEnable: get(state, ["qt", "config", "publisher-attributes", "onesignal", "is_enable"], false),
+    oneSignalAppId: get(state, ["qt", "config", "public-integrations", "one-signal", "app-id"], null)
   };
 };
 
@@ -37,7 +39,16 @@ export const getCriticalCss = async () => {
 };
 
 export async function renderLayout(res, params) {
-  const { gtmId, gaId, cdnImage, isOnesignalEnable, isGtmEnable, isGaEnable } = getConfig(params.store.getState());
+  const {
+    gtmId,
+    gaId,
+    cdnImage,
+    isOnesignalEnable,
+    oneSignalAppId,
+    oneSignalSafariId,
+    isGtmEnable,
+    isGaEnable
+  } = getConfig(params.store.getState());
   const chunk = params.shell ? null : allChunks[getChunkName(params.pageType)];
   const criticalCss = await getCriticalCss();
 
@@ -72,7 +83,8 @@ export async function renderLayout(res, params) {
         isGtmEnable,
         isGaEnable,
         isOnesignalEnable,
-        oneSignalScript: params.oneSignalScript
+        oneSignalSafariId,
+        oneSignalAppId
       },
       params
     )
