93a00aed6513d3b1b26c96d2c67b3300f1691e61
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-04-05 11:36:58 +0530

    updated script with defer

--- app/isomorphic/components/one-signal.js
+++ app/isomorphic/components/one-signal.js
@@ -4,7 +4,7 @@ export const OneSignal = () => {
   useEffect(() => {
     const myScript = document.createElement("script");
     myScript.setAttribute("src", "https://cdn.onesignal.com/sdks/OneSignalSDK.js");
-    myScript.setAttribute("async", "true");
+    myScript.setAttribute("defer", "true");
     const head = document.head;
     head.insertBefore(myScript, head.firstElementChild);
   }, []);
