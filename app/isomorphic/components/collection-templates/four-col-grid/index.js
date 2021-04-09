1b5660b81afa9b557dcbb124f2d222023cb6a3f7
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-09 15:09:30 +0530

    change ad id in four col grid

--- app/isomorphic/components/collection-templates/four-col-grid/index.js
+++ app/isomorphic/components/collection-templates/four-col-grid/index.js
@@ -6,12 +6,13 @@ import { useSelector } from "react-redux";
 import { get } from "lodash";
 
 import { StoryGrid } from "../../story-grid";
-import { DfpComponent } from "../../ads/dfp-component";
 
 import "./four-col-grid.m.css";
+import { DfpComponent } from "../../ads/dfp-component";
 
 export function FourColGrid({ collection, stories }) {
   const adConfig = useSelector(state => get(state, ["qt", "config", "ads-config", "slots", "listing_page_ads"], {}));
+  const pageType = useSelector(state => get(state, ["qt", "pageType"], null));
 
   return (
     <div>
@@ -19,7 +20,7 @@ export function FourColGrid({ collection, stories }) {
       <StoryGrid stories={stories} />
       <DfpComponent
         adStyleName="ad-slot-size-300x250"
-        id={`fourcol-${collection.slug}-ad`}
+        id={`${pageType}-listing-${collection.slug}-ad`}
         path={adConfig.ad_unit}
         size={adConfig.sizes}
         viewPortSizeMapping={adConfig.view_port_size_mapping}
