823e8942168290e28965adbef6e98443bb2513a1
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-04-12 11:33:04 +0530

    apm integration

--- start.js
+++ start.js
@@ -1,3 +1,12 @@
 const { startApp } = require("@quintype/framework/server/start");
+const elasticApm = require("elastic-apm-node");
+
+process.env.NODE_ENV === "production" &&
+  elasticApm.start({
+    serviceName: process.env.APM_SERVICE_NAME,
+    secretToken: process.env.APM_SECRET_TOKEN,
+    serverUrl: process.env.APM_SERVER_URL,
+    environment: process.env.APM_ENVIRONMENT
+  });
 
 startApp(() => require("./app/server/app.js").app);
