36b7c2fc188d51e0fd9abd5ecad09eaf05905bfb
Amogh Sahasrabhojanee <amogh@quintype.com>
2021-04-29 21:28:01 +0530

    add comments

--- app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
+++ app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
@@ -3,17 +3,17 @@ import { FourColTwelveStory } from "@quintype/arrow";
 import { object } from "prop-types";
 // import "@quintype/arrow/FourColTwelveStories/styles.arrow.css";
 
-export const ArrowFourColTwelveStories = ({collection}) => {
+// To Do:
+// FourColTwelveStory doesn't exist in arrow. Fix the name
+// fix all newly introduced linting errors
+// fix eslint & prettier on local machine so that linting errors aren't introduced
 
-  return (
-     <FourColTwelveStory
-      collection={collection}
-    />
-  )
-}
+export const ArrowFourColTwelveStories = ({ collection }) => {
+  return <FourColTwelveStory collection={collection} />;
+};
 
 ArrowFourColTwelveStories.propTypes = {
   collection: object
-}
+};
 
 ArrowFourColTwelveStories.storyLimit = 12;
