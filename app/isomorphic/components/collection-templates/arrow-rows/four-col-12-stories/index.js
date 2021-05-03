333146160c58bf5829c6d1606aadc2bb8c7411dd
Shraddha Kesari <shraddha.kesari@gmail.com>
2021-05-03 15:30:43 +0530

    fix four col 12 stories component

--- app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
+++ app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { FourColTwelveStory } from "@quintype/arrow";
+import { FourColTwelveStories } from "@quintype/arrow";
 import { object } from "prop-types";
 // import "@quintype/arrow/FourColTwelveStories/styles.arrow.css";
 
-// To Do:
-// FourColTwelveStory doesn't exist in arrow. Fix the name
-// fix all newly introduced linting errors
-// fix eslint & prettier on local machine so that linting errors aren't introduced
-
 export const ArrowFourColTwelveStories = ({ collection }) => {
-  return <FourColTwelveStory collection={collection} />;
+  return <FourColTwelveStories collection={collection} />;
 };
 
 ArrowFourColTwelveStories.propTypes = {
