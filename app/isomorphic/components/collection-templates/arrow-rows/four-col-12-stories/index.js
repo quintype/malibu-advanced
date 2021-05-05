2247f189cf207f730a7f13102ca0b70a5b687753
Shraddha Kesari <shraddha.kesari@gmail.com>
2021-05-05 12:32:50 +0530

    Fix four col 12 stories component issue

--- app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
+++ app/isomorphic/components/collection-templates/arrow-rows/four-col-12-stories/index.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { FourColTwelveStory } from "@quintype/arrow";
+import { FourColTwelveStories } from "@quintype/arrow";
 import { object } from "prop-types";
 // import "@quintype/arrow/FourColTwelveStories/styles.arrow.css";
 
-export const ArrowFourColTwelveStories = ({collection}) => {
-
-  return (
-     <FourColTwelveStory
-      collection={collection}
-    />
-  )
-}
+export const ArrowFourColTwelveStories = ({ collection }) => {
+  return <FourColTwelveStories collection={collection} />;
+};
 
 ArrowFourColTwelveStories.propTypes = {
   collection: object
-}
+};
 
 ArrowFourColTwelveStories.storyLimit = 12;
