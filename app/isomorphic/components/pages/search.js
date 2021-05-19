ee1ee06e5880efa125df0505150684f3fc99966e
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-05-19 17:19:05 +0530

    feat: integrate arrow for tag and search page

--- app/isomorphic/components/pages/search.js
+++ app/isomorphic/components/pages/search.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { get } from "lodash";
+import { OneColStoryList } from "@quintype/arrow";
+import fetch from "node-fetch";
 
-import { StoryGrid } from "../story-grid";
 import { DfpComponent } from "../ads/dfp-component";
 
 const SearchPage = props => {
   const adConfig = useSelector(state => get(state, ["qt", "config", "ads-config", "slots", "listing_page_ads"], {}));
+  const [storiesToRender, setStoriesToRender] = useState(8);
+  const [stories, setStories] = useState(props.data.stories);
+
+  const collection = {
+    items: stories.slice(0, storiesToRender)
+  };
+
+  const getMoreStories = async (offset, limit) => {
+    const { results } = await (
+      await fetch(`/api/v1/search?q=${props.data.query}&offset=${offset}&limit=${limit + 1}`)
+    ).json();
+    const loadMoreStories = results.stories.map(story => {
+      return { type: "story", story: story };
+    });
+    setStories(stories.slice(0, storiesToRender).concat(loadMoreStories));
+    setStoriesToRender(storiesToRender + offset);
+  };
 
   return (
     <div className="container">
       <h1>
         Search - {props.data.query} ({props.data.total} results)
       </h1>
-      <StoryGrid stories={props.data.stories} />
+      <OneColStoryList
+        collection={collection}
+        config={{ buttonText: "Load More", footerButton: "SubsequentLoadCount" }}
+        isLoadMoreVisible={stories.length > storiesToRender}
+        getMoreStories={getMoreStories}
+      />
+      {/* <StoryGrid stories={props.data.stories} /> */}
       <DfpComponent
         adStyleName="ad-slot-size-300x250"
         id="search-page-ad"
--- app/isomorphic/components/pages/tag.js
+++ app/isomorphic/components/pages/tag.js
@@ -1,18 +1,41 @@
 import { get } from "lodash";
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
+import { OneColStoryList } from "@quintype/arrow";
+import fetch from "node-fetch";
 
-import { StoryGrid } from "../story-grid";
 import { DfpComponent } from "../ads/dfp-component";
 
 const TagPage = props => {
   const adConfig = useSelector(state => get(state, ["qt", "config", "ads-config", "slots", "listing_page_ads"], {}));
+  const [storiesToRender, setStoriesToRender] = useState(8);
+  const [tagPageStories, setStories] = useState(props.data.stories);
+
+  const collection = {
+    items: tagPageStories.slice(0, storiesToRender)
+  };
+
+  const getMoreStories = async (offset, limit) => {
+    const { stories } = await (
+      await fetch(`/api/v1//stories?tag-slugs=${props.data.tag.slug}&offset=${offset}&limit=${limit + 1}`)
+    ).json();
+    const loadMoreStories = stories.map(story => {
+      return { type: "story", story: story };
+    });
+    setStories(tagPageStories.slice(0, storiesToRender).concat(loadMoreStories));
+    setStoriesToRender(storiesToRender + offset);
+  };
 
   return (
     <div className="container">
       <h1>{get(props, "data.tag.name") || "Tag Page"}</h1>
-      <StoryGrid stories={props.data.stories} />
+      <OneColStoryList
+        collection={collection}
+        config={{ buttonText: "Load More", footerButton: "SubsequentLoadCount" }}
+        isLoadMoreVisible={tagPageStories.length > storiesToRender}
+        getMoreStories={getMoreStories}
+      />
       <DfpComponent
         adStyleName="ad-slot-size-300x250"
         id="tag-page-ad"
