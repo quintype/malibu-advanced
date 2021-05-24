fcfcb32ebfffc1f2ca8fdddc66acd6a9c6528359
ags1773 <amogh@quintype.com>
2021-05-24 11:54:02 +0530

    wip: author page

--- app/isomorphic/components/pages/author-page/index.js
+++ app/isomorphic/components/pages/author-page/index.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { object, array, shape } from "prop-types";
+import { object, shape } from "prop-types";
 import { useSelector } from "react-redux";
-import { get } from "lodash";
-
-import { StoryGrid } from "../../story-grid";
+import get from "lodash/get";
+import { AuthorIntroductionCard } from "@quintype/arrow";
+import { ArrowThreeColGrid } from "../../collection-templates/arrow-rows";
 import { DfpComponent } from "../../ads/dfp-component";
+// import { LoadMoreHoc } from "../../hoc-wrappers";
 
-const AuthorPage = props => {
-  const stories = props.data.stories.map(({ story }) => story) || [];
-  if (!props.data.author.name) {
-    return <h1>No author found</h1>;
-  }
-
+export const AuthorPage = props => {
   const adConfig = useSelector(state => get(state, ["qt", "config", "ads-config", "slots", "listing_page_ads"], {}));
-
+  // const arrowThreeColGridWithLoadMore = (
+  //   <LoadMoreHoc Component={<ArrowThreeColGrid collection={props.data.authorCollection} />} defaultLoadCount={3} />
+  // );
+  const authorIntrCardConfig = {
+    enableBio: true,
+    enableSocialLinks: true
+  };
   return (
     <div className="container">
-      <h1>{`Author - ${props.data.author.name}`}</h1>
-      <StoryGrid stories={stories} />
+      <AuthorIntroductionCard data={props.data.author} config={authorIntrCardConfig} />
+      {/* {arrowThreeColGridWithLoadMore} */}
+      <ArrowThreeColGrid collection={props.data.authorCollection} />
       <DfpComponent
         adStyleName="ad-slot-size-300x250"
         id="author-page-ad"
@@ -32,8 +35,6 @@ const AuthorPage = props => {
 AuthorPage.propTypes = {
   data: shape({
     author: object,
-    stories: array
+    authorCollection: object
   })
 };
-
-export { AuthorPage };
