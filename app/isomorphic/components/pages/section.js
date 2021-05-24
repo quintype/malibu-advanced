0d038af748c0f28f2c5a4af075616f5a78fdf32d
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-05-24 12:40:20 +0530

    feat: implement arrow component to section, tag and search page

--- app/isomorphic/components/pages/section.js
+++ app/isomorphic/components/pages/section.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { string, shape, object } from "prop-types";
 import get from "lodash/get";
 import { collectionToStories, LazyCollection } from "@quintype/components";
 import { useSelector } from "react-redux";
+import { OneColStoryList } from "@quintype/arrow";
 
-import { StoryGrid } from "../story-grid";
 import { getCollectionTemplate } from "../get-collection-template";
 import { DfpComponent } from "../ads/dfp-component";
+import { getLoadMoreStories } from "../utils";
 
 const SectionPage = props => {
+  // will be getting initially 9 items, but showing only 8 for loadmore functionality
   const adConfig = useSelector(state => get(state, ["qt", "config", "ads-config", "slots", "listing_page_ads"], {}));
+  const shouldUseCollection = useSelector(state =>
+    get(state, ["qt", "config", "publisher-attributes", "should_use_collection"])
+  );
+  const [storiesToRender, setStoriesToRender] = useState(8);
+  const [sectionPageStories, setStories] = useState(props.data.collection.items);
+
+  const getMoreStories = async (offset, limit) => {
+    await getLoadMoreStories({
+      offset: offset,
+      limit: limit,
+      slug: props.data.section.id,
+      query: "section-id",
+      shouldUseCollection: shouldUseCollection,
+      setStories: setStories,
+      storiesToRender: storiesToRender,
+      setStoriesToRender: setStoriesToRender,
+      stories: sectionPageStories
+    });
+  };
 
   const stories =
     (props.data.collection && props.data.collection.items && collectionToStories(props.data.collection)) || [];
@@ -24,6 +45,11 @@ const SectionPage = props => {
   const childCollections = (get(props, ["data", "collection", "items"]) || []).filter(
     item => item.type === "collection" && item.items.length > 0
   );
+
+  const collection = {
+    items: !childCollections.length ? sectionPageStories.slice(0, storiesToRender) : props.data.collection.items
+  };
+
   const pageTitle =
     props.pageType === "collection-page"
       ? props.data.collection.name
@@ -32,7 +58,12 @@ const SectionPage = props => {
   return (
     <div className="container">
       <h1>{pageTitle}</h1>
-      <StoryGrid stories={stories} />
+      <OneColStoryList
+        collection={collection}
+        config={{ buttonText: "Load More", footerButton: "SubsequentLoadCount" }}
+        isLoadMoreVisible={!childCollections.length && sectionPageStories.length > storiesToRender}
+        getMoreStories={getMoreStories}
+      />
       <DfpComponent
         adStyleName="ad-slot-size-300x250"
         id="section-page-ad"
