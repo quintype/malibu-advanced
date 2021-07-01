54830a99d91568c42b53a4615e93a3006085ecc6
Athira MR <37729206+Athira001@users.noreply.github.com>
2021-07-01 17:06:50 +0530

    fix(story page) Refactor story page 🔨  (#195)
    * fix(story page) Refactor story page
    
    * fix(update) :Lhci assertions

--- app/isomorphic/components/layouts/app-logo/index.js
+++ app/isomorphic/components/layouts/app-logo/index.js
@@ -3,18 +3,14 @@ import { Link } from "@quintype/components";
 
 import "./app-logo.m.css";
 
-const AppLogo = () => (
-  <Link href="/">
-    <img
-      height="36"
-      width="178"
-      loading="lazy"
-      styleName="publisher-logo"
-      srcSet="https://qtstage-01.gumlet.io/malibu/2021-03/193b1a09-89cf-4b07-8d4f-fcf00d534577/malibu_logo_new.svg"
-      data-src="https://qtstage-01.gumlet.io/malibu/2021-03/193b1a09-89cf-4b07-8d4f-fcf00d534577/malibu_logo_new.svg"
-      alt="Logo"
-    />
-  </Link>
-);
+import { SvgIconHandler } from "../../atoms/svg-icon-hadler/index";
+
+const AppLogo = () => {
+  return (
+    <Link href="/">
+      <SvgIconHandler type="logo" width="178" height="36" viewBox="0 0 997.64 195.19" />
+    </Link>
+  );
+};
 
 export { AppLogo };
