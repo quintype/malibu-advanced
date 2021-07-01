302d61b10755d1f1b4538c826366696eb3661c14
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-07-01 09:05:49 +0530

    make authorise call post login in auth domain

--- app/isomorphic/components/pages/user-login/index.js
+++ app/isomorphic/components/pages/user-login/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AccountModal from "../../login/AccountModal";
 
+function useQuery() {
+  const urlObj2 = new URL(window.location.href);
+  const urlSubstring2 = urlObj2.search;
+
+  return new URLSearchParams(urlSubstring2);
+}
+
+const foo = async () => {
+  const integrationId = 51;
+  const redirectUri = `https://malibu-advanced-web.qtstage.io/user/signup`;
+
+  const params = `client_id=${integrationId}&redirect_uri=${redirectUri}&response_type=code&allow_ajax=true`;
+  const url = `/api/auth/v1/oauth/authorize?${params}`;
+  const res = await window.fetch(url, {
+    method: "GET"
+  });
+  if (res) {
+    if (res.status === 200) {
+      const response = await res.json();
+      window.location.href = response.redirect_uri;
+    } else {
+      const response = await res.json();
+      window.alert(response.error_description);
+    }
+  }
+};
+
 const UserLoginPage = () => {
+  useEffect(() => {
+    const queryParams = useQuery();
+    const redirectUrl = queryParams.get("redirect_uri");
+    if (redirectUrl) {
+      foo();
+    }
+  }, []);
   return <AccountModal isPopup={false} />;
 };
 
