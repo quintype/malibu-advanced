6b03dcdd1589f6f89ff55a714c1bf4943d568db1
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2021-07-02 16:10:42 +0530

    feat: added profile information

--- app/isomorphic/components/pages/profile-page/index.js
+++ app/isomorphic/components/pages/profile-page/index.js
@@ -13,22 +13,25 @@ const ProfilePage = () => {
 
   return (
     <div styleName="profile-page">
-      <div styleName="avatar">
-        {member["avatar-url"] ? (
-          <img src={member["avatar-url"]} alt="avatar" height="160px" width="160px" styleName="avatar-image" />
-        ) : (
-          <SvgIconHandler type="user" styleName="user-icon" />
-        )}
-      </div>
-      <div styleName="profile-information">
-        <p styleName="fields">
-          <strong>Name: </strong>
-          {member.name}
-        </p>
-        <p styleName="fields">
-          <strong>Email: </strong>
-          {member.email}
-        </p>
+      <div styleName="my-account">My Account</div>
+      <div styleName="profile-card">
+        <div styleName="avatar">
+          {member["avatar-url"] ? (
+            <img src={member["avatar-url"]} alt="avatar" height="160px" width="160px" styleName="avatar-image" />
+          ) : (
+            <SvgIconHandler type="user" styleName="user-icon" />
+          )}
+        </div>
+        <div styleName="profile-information">
+          <p styleName="fields">
+            <strong>Name: </strong>
+            {member.name}
+          </p>
+          <p styleName="fields">
+            <strong>Email: </strong>
+            {member.email}
+          </p>
+        </div>
       </div>
     </div>
   );
