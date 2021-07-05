b95b36950a8f16a8462f6fb1b7c8b2efbf215237
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-07-05 13:59:03 +0530

    revert signupHandler

--- app/isomorphic/components/pages/user-signup/index.js
+++ app/isomorphic/components/pages/user-signup/index.js
@@ -1,7 +1,7 @@
-// import { get } from "lodash";
+import { get } from "lodash";
 import React, { useEffect } from "react";
 // import { useSelector } from "react-redux";
-// import axios from "axios";
+import axios from "axios";
 
 function useQuery() {
   const urlObj2 = new URL(window.location.href);
@@ -10,55 +10,55 @@ function useQuery() {
   return new URLSearchParams(urlSubstring2);
 }
 
-// const getAccessToken = async (authCode, brkeConfig) => {
-//   const { bridgekeeperIntegrationId, bridgekeeperIntegrationSecret, bridgekeeperHost, bridgekeeperApiKey } = brkeConfig;
+const getAccessToken = async (authCode, brkeConfig) => {
+  const { bridgekeeperIntegrationId, bridgekeeperIntegrationSecret, bridgekeeperHost, bridgekeeperApiKey } = brkeConfig;
 
-//   if (!bridgekeeperIntegrationId || !bridgekeeperIntegrationSecret || !bridgekeeperHost) {
-//     return Promise.reject(new Error("Unable to get accessToken: invalid bridgekeeper config"));
-//   }
+  if (!bridgekeeperIntegrationId || !bridgekeeperIntegrationSecret || !bridgekeeperHost) {
+    return Promise.reject(new Error("Unable to get accessToken: invalid bridgekeeper config"));
+  }
 
-//   const tokenUrl = `${bridgekeeperHost}/api/auth/v1/oauth/token`;
+  const tokenUrl = `${bridgekeeperHost}/api/auth/v1/oauth/token`;
 
-//   const form = new URLSearchParams();
-//   form.append("client_id", bridgekeeperIntegrationId);
-//   form.append("client_secret", bridgekeeperIntegrationSecret);
-//   form.append("grant_type", "authorization_code");
-//   form.append("code", authCode);
+  const form = new URLSearchParams();
+  form.append("client_id", bridgekeeperIntegrationId);
+  form.append("client_secret", bridgekeeperIntegrationSecret);
+  form.append("grant_type", "authorization_code");
+  form.append("code", authCode);
 
-//   try {
-//     const requestTokenResponse = await axios.post(tokenUrl, form, {
-//       headers: { "Content-Type": "application/x-www-form-urlencoded", "X-BK-AUTH": bridgekeeperApiKey }
-//     });
-//     const accessToken = get(requestTokenResponse, ["data", "access_token"]);
-//     return accessToken;
-//   } catch (err) {
-//     console.log(`Request for Access Token Failed:${err}`);
-//   }
-// };
+  try {
+    const requestTokenResponse = await axios.post(tokenUrl, form, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded", "X-BK-AUTH": bridgekeeperApiKey }
+    });
+    const accessToken = get(requestTokenResponse, ["data", "access_token"]);
+    return accessToken;
+  } catch (err) {
+    console.log(`Request for Access Token Failed:${err}`);
+  }
+};
 
-// const signupHandler = async code => {
-//   const bridgekeeperIntegrationId = 51;
-//   const bridgekeeperIntegrationSecret = "kcRFrsf89as8fHkfhHihbg3LMHjii8";
-//   const bridgekeeperHost = "http://bridgekeeper.alb.staging.quinpress.internal";
-//   const bridgekeeperApiKey = "mxdnwJFPrZUQDPuV";
+const signupHandler = async code => {
+  const bridgekeeperIntegrationId = 51;
+  const bridgekeeperIntegrationSecret = "kcRFrsf89as8fHkfhHihbg3LMHjii8";
+  const bridgekeeperHost = "http://bridgekeeper.alb.staging.quinpress.internal";
+  const bridgekeeperApiKey = "mxdnwJFPrZUQDPuV";
 
-//   const brkeConfig = { bridgekeeperIntegrationId, bridgekeeperIntegrationSecret, bridgekeeperHost, bridgekeeperApiKey };
+  const brkeConfig = { bridgekeeperIntegrationId, bridgekeeperIntegrationSecret, bridgekeeperHost, bridgekeeperApiKey };
 
-//   try {
-//     const accessToken = await getAccessToken(code, brkeConfig);
-//     const cookieConf = { httpOnly: true };
-//     if (process.env.NODE_ENV !== "development") {
-//       cookieConf.secure = true;
-//     }
-//     console.log("fooooo accessToken 111111", accessToken);
+  try {
+    const accessToken = await getAccessToken(code, brkeConfig);
+    const cookieConf = { httpOnly: true };
+    if (process.env.NODE_ENV !== "development") {
+      cookieConf.secure = true;
+    }
+    console.log("fooooo accessToken 111111", accessToken);
 
-//     // res.cookie("token", accessToken, cookieConf);
-//     // return res.send({});
-//   } catch (err) {
-//     console.log(err);
-//     // return res.send({ error: "User authentication failed" });
-//   }
-// };
+    // res.cookie("token", accessToken, cookieConf);
+    // return res.send({});
+  } catch (err) {
+    console.log(err);
+    // return res.send({ error: "User authentication failed" });
+  }
+};
 
 export const UserSignupPage = () => {
   // const member = useSelector(state => get(state, ["member"], null));
@@ -68,7 +68,7 @@ export const UserSignupPage = () => {
       const queryParams = useQuery();
       const code = queryParams.get("code");
       console.log("fooooo code", code);
-      // signupHandler(code);
+      signupHandler(code);
       // await wretch(`/api/v1/accounts/signup`).post({ code });
     };
 
