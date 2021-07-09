7452a23700ffeeb09a75b1cfd4cac4627d834efb
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2021-07-09 14:42:52 +0530

    work in progress (edit_profile)

--- app/isomorphic/components/pages/profile-page/index.js
+++ app/isomorphic/components/pages/profile-page/index.js
@@ -10,9 +10,78 @@ import { MEMBER_UPDATED } from "../../store/actions";
 
 import "./profile-page.m.css";
 
+function signImage(fileName, mimeType) {
+  const url = `/api/sign?file-name=${fileName}&mime-type=${mimeType}`;
+  return fetch(url).then(res => res.json());
+}
+
+const uploadS3ToTemp = async (res, formdata) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: formdata,
+    redirect: "follow"
+  };
+
+  return fetch(`${res.action}`, requestOptions)
+    .then(response => {
+      return response;
+    })
+    .catch(err => console.error(err));
+};
+
 const EditProfile = ({ setIsEditing, isEditing }) => {
+  const [tempImageObj, setTempImageObj] = useState(null);
   const [name, setName] = useState("");
   const dispatch = useDispatch();
+
+  const prepareFormData = (keys, formdata, imageList, res) => {
+    return keys.map(key => {
+      if (key === "file") {
+        return formdata.append(key, imageList[0].name);
+      } else {
+        return formdata.append(key, res[key]);
+      }
+    });
+  };
+
+  const onProfileChange = event => {
+    const imageList = event.target.files;
+    signImage(imageList[0].name, imageList[0].type).then(res => {
+      // eslint-disable-next-line no-undef
+      const formdata = new FormData();
+      formdata.append("name", "phani");
+
+      const keys = [
+        "key",
+        "Content-Type",
+        "policy",
+        "acl",
+        "success_action_status",
+        "AWSAccessKeyId",
+        "signature",
+        "file"
+      ];
+
+      prepareFormData(keys, formdata, imageList, res);
+      console.log("FormData is --------", formdata);
+
+      uploadS3ToTemp(res, formdata).then(response => {
+        if (response.status === 201) {
+          updateUserProfile({ "temp-s3-key": res.key })
+            .then(resp => {
+              if (!resp) {
+                return;
+              }
+              setTempImageObj(resp);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      });
+    });
+  };
 
   const getCurrentUser = async () => {
     try {
@@ -25,7 +94,7 @@ const EditProfile = ({ setIsEditing, isEditing }) => {
 
   const onSubmitHandler = e => {
     e.preventDefault();
-    updateUserProfile({ name })
+    updateUserProfile({ name, "avatar-url": tempImageObj })
       .then(getCurrentUser)
       .then(() => setIsEditing(!isEditing))
       .catch(err => console.error(err));
@@ -41,6 +110,7 @@ const EditProfile = ({ setIsEditing, isEditing }) => {
         <div styleName="fields">
           <label>Name: </label>
           <input name="Name" type="text" value={name} onChange={handleChange} />
+          <input name="File" type="file" onChange={onProfileChange} />
         </div>
         <input type="submit" value="Submit" />
       </form>
