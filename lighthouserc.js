565f819c89ef83cf158d9221c33f77193ca635d0
Athira MR <athiramraju001@gmail.com>
2021-07-09 14:15:57 +0530

    fix(wip)

--- lighthouserc.js
+++ lighthouserc.js
@@ -26,6 +26,7 @@ const lhciConfig = {
         "no-document-write": "warn", // Avoid `document.write()`
         "link-name": "warn",
         "image-size-responsive": "warn",
+        "image-aspect-ratio": "warn",
         "button-name": "warn", // It suggests that the button should have inner text content or an aria-label or aria-labelledBy.
         "categories:performance": ["error", { minScore: 0.7 }],
         "image-alt": "warn",
