929186662680fa77d945a4b6a14223b695d02923
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-07-21 15:19:28 +0530

    Added ImageTags to generators

--- app/server/app.js
+++ app/server/app.js
@@ -14,6 +14,7 @@ import { pickComponent } from "../isomorphic/pick-component";
 import {
   generateStaticData,
   generateStructuredData,
+  ImageTags,
   SEO,
   StaticTags,
   StoryAmpTags,
@@ -61,7 +62,7 @@ function generateSeo(config, pageType) {
     enableOgTags: true,
     enableNews: true,
     enableFbApplink: true,
-    generators: [TextTags, StaticTags, StructuredDataTags, StoryAmpTags]
+    generators: [ImageTags, TextTags, StaticTags, StructuredDataTags, StoryAmpTags]
   });
 }
 
