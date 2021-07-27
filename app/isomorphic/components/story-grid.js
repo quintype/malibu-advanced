808f37c87d8893922d2e260d088b39b9734711bb
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-07-27 10:00:12 +0530

    add placeholder logic to story grid component

--- app/isomorphic/components/story-grid.js
+++ app/isomorphic/components/story-grid.js
@@ -6,7 +6,11 @@ import "./story-grid.m.css";
 function StoryGridStoryItem(props) {
   return (
     <Link href={`/${props.story.slug}`} className="story-grid-item">
-      <figure className="qt-image-16x9" styleName="story-grid-item-image">
+      <figure
+        className="qt-image-16x9"
+        styleName="story-grid-item-image"
+        style={{ background: global.showPlaceholder ? "lightgray" : "transparent" }}
+      >
         <ResponsiveImage
           slug={props.story["hero-image-s3-key"]}
           metadata={props.story["hero-image-metadata"]}
