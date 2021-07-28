bc74b7f17d73eeed5df6edb2c2e0c4878c9aabf6
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-07-28 10:01:57 +0530

    push reducers

--- app/isomorphic/components/store/reducers.js
+++ app/isomorphic/components/store/reducers.js
@@ -37,9 +37,19 @@ function loginReducer(state = false, action) {
   }
 }
 
+function placeHolderReducer(state = true, action) {
+  switch (action.type) {
+    case "SHOW_PLACEHOLDER":
+      return action.isPlaceHolder;
+    default:
+      return state;
+  }
+}
+
 export const REDUCERS = {
   isHamburgerMenuOpen: hamburgerMenuReducer,
   isSearchBarOpen: searchBarReducer,
   member: memberReducer,
-  isLoginOpen: loginReducer
+  isLoginOpen: loginReducer,
+  isPlaceHolder: placeHolderReducer
 };
