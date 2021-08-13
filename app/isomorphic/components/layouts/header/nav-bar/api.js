05bee223ed62202c1375b92b0fcd36d0ca8d6ab3
Athira MR <athiramraju001@gmail.com>
2021-08-13 14:50:07 +0530

    fix(wip)

--- app/isomorphic/components/layouts/header/nav-bar/api.js
+++ app/isomorphic/components/layouts/header/nav-bar/api.js
@@ -3,7 +3,7 @@ export function oauthAuthorize(integrationId, redirectUrl, callbackUrl) {
   return wretch()
     .options({ credentials: "include" })
     .url(`/api/auth/v1/oauth/authorize`)
-    .query({ client_id: integrationId, redirect_uri: redirectUrl, response_type: "code" })
+    .query({ client_id: integrationId, redirect_uri: redirectUrl, response_type: "code", allow_ajax: true })
     .get()
     .json(res => {
       return res;
