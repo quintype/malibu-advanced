79ebc60c6ea39dbe9cf1e00a8445fb17133a1480
Athira MR <athiramraju001@gmail.com>
2021-08-24 13:59:26 +0530

    fix(remove commented line)

--- app/isomorphic/components/pages/user-login/index.js
+++ app/isomorphic/components/pages/user-login/index.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import AccountModal from "../../login/AccountModal";
 
-// import { generateRedirect, getQueryParam } from "../../utils";
-
 const UserLoginPage = () => {
   return <AccountModal isPopup={false} />;
 };
