c4e907ba0b534dabed3845680b0d10882abdf2ff
Athira MR <athiramraju001@gmail.com>
2021-08-26 08:29:58 +0530

    fix(wip)

--- app/isomorphic/components/molecules/SocialLogin/index.js
+++ app/isomorphic/components/molecules/SocialLogin/index.js
@@ -23,7 +23,7 @@ export const SocialLoginBase = ({ googleAppId, facebookAppId }) => {
     const getRedirectUrl =
       get(params, ["query", "redirect_uri"]) || get(publisherAttributes, ["sso_login", "redirect_Url"], "");
     const location = new URL(window.location.href);
-    const oauthAuthorize = `${location.origin}/api/auth/v1/oauth/authorize?client_id=${clientId}&redirect_uri=${getRedirectUrl}&callback_uri=${getCallbackUrl}&response_type=code`;
+    const oauthAuthorize = `${location.origin}/api/auth/v1/oauth/authorize?redirect_uri=${getRedirectUrl}&client_id=${clientId}&callback_uri=${getCallbackUrl}&response_type=code`;
     console.log("oauthAuthorize-----------sociallogin", oauthAuthorize);
     setRedirectUrl(ssoLoginIsEnable ? oauthAuthorize : `${location.origin}${location.pathname}`);
   }, []);
