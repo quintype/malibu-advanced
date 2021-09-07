1a55507a7c37bd2b9de1fc6dde015d1a93695057
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2021-09-07 17:54:10 +0530

    added discernible name to Links

--- app/isomorphic/components/layouts/app-logo/index.js
+++ app/isomorphic/components/layouts/app-logo/index.js
@@ -8,7 +8,7 @@ import { SvgIconHandler } from "../../atoms/svg-icon-hadler/index";
 
 const AppLogo = ({ width = "178" }) => {
   return (
-    <Link href="/">
+    <Link href="/" aria-label="app-logo">
       <SvgIconHandler type="logo" width="178" height="36" viewBox="0 0 997.64 195.19" />
     </Link>
   );
