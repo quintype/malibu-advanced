7e0f14b74b227b00384528d0271fcc6f28911667
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-10-26 23:37:52 +0530

    fix assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,8 @@ const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 const path = require("path");
 const webpackConfig = require("@quintype/build/config/webpack");
 
-const resolveConfig = {
-  resolve: {
-    alias: {
-      "react-is": path.resolve("./node_modules/react-is"),
-      "lodash-es": "lodash",
-      "lodash.debounce": "lodash/debounce",
-      "@quintype/components": path.resolve("./node_modules/@quintype/components"),
-      react: path.resolve("./node_modules/react"),
-      "react-redux": path.resolve("./node_modules/react-redux"),
-      redux: path.resolve("./node_modules/redux"),
-      warning: path.resolve("./node_modules/warning"),
-      "hoist-non-react-statics": path.resolve("./node_modules/hoist-non-react-statics")
-    }
-  }
-};
 const { plugins, output, module: webpackModule } = webpackConfig;
-const libraryDef = {
-  library: "QT_COMPONENTS",
-  libraryTarget: "window"
-};
 if (process.env.NODE_ENV !== "production") output.path = path.resolve("./public");
-const customOutput = { ...output, ...libraryDef };
 const getSpritePlugin = () => new SpriteLoaderPlugin({ plainSprite: true });
 const insertIntoIndex = (arr, index, newItem) => [...arr.slice(0, index), newItem, ...arr.slice(index)];
 const enhancedPlugins = insertIntoIndex(plugins, 1, getSpritePlugin());
@@ -42,6 +22,7 @@ const spriteRule = {
     "svgo-loader"
   ]
 };
+
 const enhancedRules = insertIntoIndex(webpackModule.rules, 5, spriteRule);
 enhancedRules[8] = {
   test: /\.(jpe?g|gif|png|woff|woff2|eot|ttf|wav|mp3|ico|mp4)$/,
@@ -49,18 +30,8 @@ enhancedRules[8] = {
   options: { context: "./app/assets", name: "[name].[ext]" }
 };
 
-const externals = {
-  // Use external version of React & alike for the components library.
-  react: "React",
-  "react-dom": "ReactDOM",
-  "react-router": "ReactRouter",
-  "react-redux": "ReactRedux"
-};
 module.exports = {
   ...webpackConfig,
-  ...resolveConfig,
   module: { ...webpackModule, ...{ rules: enhancedRules } },
-  output: customOutput,
-  externals,
   plugins: enhancedPlugins
 };
