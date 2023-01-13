dfc1e60f20da3aa697bb4dc269c0a394ceaf167b
archanaknayanar <archanaknayanar@gmail.com>
2023-01-13 15:56:08 +0400

    fix lint

--- app/isomorphic/components/story-templates/text-story/index.js
+++ app/isomorphic/components/story-templates/text-story/index.js
@@ -3,7 +3,7 @@ import TextStoryTemplate from "../../../arrow/components/Rows/StoryTemplates/Tex
 import { object, func } from "prop-types";
 
 const TextStory = ({ story, config, adWidget, adPlaceholder }) => {
-  const templateSpecific = {templateType: "hero-vertical-priority", authorDetails: { template: "centerAligned" } };
+  const templateSpecific = { templateType: "hero-vertical-priority", authorDetails: { template: "centerAligned" } };
 
   return (
     <TextStoryTemplate
