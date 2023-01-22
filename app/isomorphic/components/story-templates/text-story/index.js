30953c36058f7c549e9a8fa2bc0729bbf8d433bf
archanaknayanar <archanaknayanar@gmail.com>
2023-01-22 11:50:55 +0400

    change template

--- app/isomorphic/components/story-templates/text-story/index.js
+++ app/isomorphic/components/story-templates/text-story/index.js
@@ -4,7 +4,7 @@ import { object, func } from "prop-types";
 
 const TextStory = ({ story, config, adWidget, adPlaceholder }) => {
   const templateSpecific = {
-    templateType: "hero-priority-center",
+    templateType: "headline-hero-priority",
     authorDetails: { template: "centerAligned" },
     publishedDetails: {
       enablePublishedTime: true,
