79f072b4f61a987a432af1eccb0c4f1288624752
archanaknayanar <archanaknayanar@gmail.com>
2023-03-02 13:19:40 +0530

    headline hero prio

--- app/isomorphic/components/story-templates/text-story/index.js
+++ app/isomorphic/components/story-templates/text-story/index.js
@@ -3,7 +3,7 @@ import TextStoryTemplate from "../../../arrow/components/Rows/StoryTemplates/Tex
 import { object, func } from "prop-types";
 
 const TextStory = ({ story, config, adWidget, adPlaceholder }) => {
-  const templateSpecific = { templateType: "hero-priority-center" };
+  const templateSpecific = { templateType: "headline-hero-priority" };
   return (
     <TextStoryTemplate
       story={story}
