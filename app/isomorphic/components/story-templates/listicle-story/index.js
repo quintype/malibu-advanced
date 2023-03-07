dcaf9eb371087a4c7a89e9fcf766b4b187b1a5e4
VeenaYemmiganur <104062026+VeenaYemmiganur@users.noreply.github.com>
2023-03-07 11:52:13 +0530

    Arrow aside collections (#476)
     fix. Passing asideCollection - templateConfig to story templates

--- app/isomorphic/components/story-templates/listicle-story/index.js
+++ app/isomorphic/components/story-templates/listicle-story/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ListicleStoryTemplate from "../../../arrow/components/Rows/StoryTemplates/ListicleStoryTemplates";
-import { object } from "prop-types";
+import { object, func } from "prop-types";
 
-const ListicleStory = ({ story }) => {
-  return <ListicleStoryTemplate story={story} />;
+const ListicleStory = ({ story, config, adWidget, adPlaceholder }) => {
+  const templateSpecific = { templateType: "headline-sideway" };
+
+  return (
+    <ListicleStoryTemplate
+      story={story}
+      config={{ ...config, ...templateSpecific }}
+      adComponent={adWidget}
+      widgetComp={adWidget}
+      firstChild={adPlaceholder}
+      secondChild={adPlaceholder}
+    />
+  );
 };
 
 ListicleStory.propTypes = {
   story: object,
+  config: object,
+  adWidget: func,
+  adPlaceholder: object,
 };
 
-export default React.memo(ListicleStory);
+export default ListicleStory;
