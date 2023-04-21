4440a008d4e480dc111a004e0ac69acb7ff06fcf
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2023-04-21 12:01:37 +0530

    pass story page slots

--- app/server/app.js
+++ app/server/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console, no-unused-vars, import/extensions, object-shorthand, global-require */
+import React from "react";
 import createApp from "@quintype/framework/server/create-app";
 import { getClient, Collection } from "@quintype/framework/server/api-client";
 import logger from "@quintype/framework/server/logger";
@@ -7,7 +8,7 @@ import {
   isomorphicRoutes,
   staticRoutes,
   ampRoutes,
-  getWithConfig
+  getWithConfig,
 } from "@quintype/framework/server/routes";
 import { generateRoutes, STATIC_ROUTES } from "./routes";
 import { renderLayout } from "./handlers/render-layout";
@@ -19,44 +20,51 @@ export const app = createApp();
 
 upstreamQuintypeRoutes(app, {});
 
-const redirectCollectionHandler = () => async (req, res, next, { client, config }) => {
-  const response = await Collection.getCollectionBySlug(client, req.params.collectionSlug, { limit: 20 }, { depth: 2 });
-  if (!response) {
+const redirectCollectionHandler =
+  () =>
+  async (req, res, next, { client, config }) => {
+    const response = await Collection.getCollectionBySlug(
+      client,
+      req.params.collectionSlug,
+      { limit: 20 },
+      { depth: 2 }
+    );
+    if (!response) {
+      return next();
+    }
+    const collection = response && response.collection;
+    if (collection.template === "section") {
+      const sectionId = collection.metadata.section[0].id;
+      const section = config.sections.find((section) => section.id === sectionId) || {};
+      return res.redirect(301, `${section["section-url"]}`);
+    }
+
+    if (collection.template === "author") {
+      return res.redirect(301, `/author/${req.params.collectionSlug}`);
+    }
     return next();
-  }
-  const collection = response && response.collection;
-  if (collection.template === "section") {
-    const sectionId = collection.metadata.section[0].id;
-    const section = config.sections.find(section => section.id === sectionId) || {};
-    return res.redirect(301, `${section["section-url"]}`);
-  }
+  };
 
-  if (collection.template === "author") {
-    return res.redirect(301, `/author/${req.params.collectionSlug}`);
-  }
-  return next();
-};
-
-const logError = error => logger.error(error);
+const logError = (error) => logger.error(error);
 
 getWithConfig(app, "/collection/:collectionSlug", redirectCollectionHandler(), {
-  logError
+  logError,
 });
 
 function returnConfig(req) {
   return getClient(req.hostname)
     .getConfig()
-    .then(res => res.config || []);
+    .then((res) => res.config || []);
 }
 
 app.get("*", (req, res, next) => {
   if (req.hostname.includes("auth")) {
     const whitelistedUrls = ["user-login", "route-data.json", "manifest.json"];
-    const isPathPresent = element => req.params[0].includes(element);
+    const isPathPresent = (element) => req.params[0].includes(element);
     if (whitelistedUrls.some(isPathPresent)) {
       return next();
     } else {
-      returnConfig(req).then(response => {
+      returnConfig(req).then((response) => {
         const sketchesHost = response["sketches-host"];
         res.redirect(301, sketchesHost);
       });
@@ -72,21 +80,33 @@ function generateSeo(config, pageType) {
     structuredData: Object.assign(generateStructuredData(config), {
       enableLiveBlog: true,
       enableVideo: true,
-      enableNewsArticle: true
+      enableNewsArticle: true,
     }),
     enableTwitterCards: true,
     enableOgTags: true,
-    enableNews: true
+    enableNews: true,
   });
 }
 
 ampRoutes(app, {
-  seo: generateSeo
+  seo: generateSeo,
+  slots: {
+    story: {
+      "default-story-card-slot": ({ story, config, index, card }) => (
+        <div style={{ padding: "10px", backgroundColor: "yellow" }}>Slot after card {index}</div>
+      ),
+      "default-story-element-slot": ({ story, config, cardIdx, storyElementIdx, storyElement }) => (
+        <div style={{ padding: "10px", backgroundColor: "aquamarine" }}>
+          Slot after card: {cardIdx} and storyElement: {storyElementIdx}
+        </div>
+      ),
+    },
+  },
 });
 
 isomorphicRoutes(app, {
   appVersion: require("../isomorphic/app-version"),
-  logError: error => logger.error(error),
+  logError: (error) => logger.error(error),
   generateRoutes: generateRoutes,
   loadData: loadData,
   pickComponent: pickComponent,
@@ -97,5 +117,5 @@ isomorphicRoutes(app, {
   seo: generateSeo,
   preloadJs: true,
   oneSignalServiceWorkers: true,
-  prerenderServiceUrl: "https://prerender.quintype.io"
+  prerenderServiceUrl: "https://prerender.quintype.io",
 });
