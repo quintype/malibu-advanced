21a9108d9a9716e8a3d025cad5bad95bdc8816f6
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-05-20 00:53:48 +0530

    add isAmpSubscriptionsEnabled to structured data

--- app/server/app.js
+++ app/server/app.js
@@ -80,6 +80,7 @@ function generateSeo(config, pageType) {
       enableLiveBlog: true,
       enableVideo: true,
       enableNewsArticle: true,
+      isAmpSubscriptionsEnabled: true,
     }),
     fallbackSocialImage:
       "https://gumlet.assettype.com/malibu/2023-04/59d3e1a7-019f-46eb-ba51-03989f32c4c1/birds_7901303_960_720.jpeg",
