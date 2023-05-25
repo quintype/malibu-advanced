be9a47903adc0ea64e613ba61ef76d1f7e7cf3e6
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-05-25 13:14:13 +0530

    fix lint_js

--- app/isomorphic/arrow/components/Rows/StoryTemplates/TextStoryTemplates/index.js
+++ app/isomorphic/arrow/components/Rows/StoryTemplates/TextStoryTemplates/index.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import kebabCase from "lodash/kebabCase";
 import { StateProvider } from "../../../SharedContext";
