8df9267c0ac01ad73f1e2eb28a01e9e971720dc0
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-05-31 16:37:34 +0530

    pass coupon code to razorpay method

--- app/isomorphic/components/pages/subscription-page/Modals/LoginModal/index.js
+++ app/isomorphic/components/pages/subscription-page/Modals/LoginModal/index.js
@@ -10,39 +10,38 @@ const currencyLabels = {
 export const LoginModal = ({ setActiveTab, AccountModal, selectedPlan }) => {
   const [showAccountModal, setShowAccountModal] = useState(true);
 
-  return <button onClick={() => setActiveTab("checkout")}>Dummy Login</button>;
-  // const { plan } = selectedPlan;
-  // return (
-  //   <div styleName="modal">
-  //     <div styleName="checkout-label">Checkout</div>
-  //     <div styleName="secure">100% secure</div>
-  //     <div styleName="current-step">Step 1 of 2</div>
-  //     <div styleName="login-modal">
-  //       {showAccountModal && (
-  //         <AccountModal
-  //           isPopup={false}
-  //           customCallbackUrl={global.location.href}
-  //           onClose={() => {
-  //             setShowAccountModal(false);
-  //             setActiveTab("checkout");
-  //           }}
-  //         />
-  //       )}
-  //     </div>
-  //     <div styleName="plan-preview">
-  //       <div styleName="label">Plan Details</div>
-  //       <div styleName="plan-details">
-  //         <div styleName="plan-name-and-price">
-  //           <div styleName="group-plan-name">{`${plan.title} - ${
-  //             plan.recurring ? "Recurring Plan" : "One Time Plan"
-  //           }`}</div>
-  //           <div styleName="price">{`${currencyLabels[plan.price_currency]} ${plan.price_cents / 100}/-`}</div>
-  //         </div>
-  //       </div>
-  //       <div styleName="validity">{`Valid for ${plan.duration_length} ${plan.duration_unit}`}</div>
-  //     </div>
-  //   </div>
-  // );
+  const { plan } = selectedPlan;
+  return (
+    <div styleName="modal">
+      <div styleName="checkout-label">Checkout</div>
+      <div styleName="secure">100% secure</div>
+      <div styleName="current-step">Step 1 of 2</div>
+      <div styleName="login-modal">
+        {showAccountModal && (
+          <AccountModal
+            isPopup={false}
+            customCallbackUrl={global.location.href}
+            onClose={() => {
+              setShowAccountModal(false);
+              setActiveTab("checkout");
+            }}
+          />
+        )}
+      </div>
+      <div styleName="plan-preview">
+        <div styleName="label">Plan Details</div>
+        <div styleName="plan-details">
+          <div styleName="plan-name-and-price">
+            <div styleName="group-plan-name">{`${plan.title} - ${
+              plan.recurring ? "Recurring Plan" : "One Time Plan"
+            }`}</div>
+            <div styleName="price">{`${currencyLabels[plan.price_currency]} ${plan.price_cents / 100}/-`}</div>
+          </div>
+        </div>
+        <div styleName="validity">{`Valid for ${plan.duration_length} ${plan.duration_unit}`}</div>
+      </div>
+    </div>
+  );
 };
 
 LoginModal.propTypes = {
