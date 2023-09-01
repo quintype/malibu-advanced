70a8511e1405e80f62c3005ab472289481c4d147
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-09-01 15:30:02 +0530

    uncomment all the metype components

--- app/isomorphic/components/Metype/contribution-widget/index.js
+++ app/isomorphic/components/Metype/contribution-widget/index.js
@@ -1,32 +1,30 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect } from "react";
 import { scriptLoader } from "../index.js";
 import { string, number } from "prop-types";
 
 export const MetypeContributionWidget = ({ host, accountId, publisher, fontFamily, fontUrl }) => {
-  // const randomNumber = new Date().getMilliseconds();
-  // useEffect(() => {
-  //   !window.talktype && scriptLoader(host, () => initWidget(randomNumber));
-  //   initWidget(randomNumber);
-  // }, []);
-  // const initWidget = (randomNumber) => {
-  //   if (window.talktype) {
-  //     window.talktype.contributionWidgetIframe(document.getElementById(`metype-contribution-${randomNumber}`));
-  //   }
-  // };
-  // return (
-  //   <div>
-  //     <div
-  //       id={`metype-contribution-${randomNumber}`}
-  //       data-metype-account-id={accountId}
-  //       data-metype-host={host}
-  //       data-metype-publisher={publisher}
-  //       data-metype-font-url={fontUrl || ""}
-  //       data-metype-font-family={fontFamily || ""}
-  //     ></div>
-  //   </div>
-  // );
-  return null;
+  const randomNumber = new Date().getMilliseconds();
+  useEffect(() => {
+    !window.talktype && scriptLoader(host, () => initWidget(randomNumber));
+    initWidget(randomNumber);
+  }, []);
+  const initWidget = (randomNumber) => {
+    if (window.talktype) {
+      window.talktype.contributionWidgetIframe(document.getElementById(`metype-contribution-${randomNumber}`));
+    }
+  };
+  return (
+    <div>
+      <div
+        id={`metype-contribution-${randomNumber}`}
+        data-metype-account-id={accountId}
+        data-metype-host={host}
+        data-metype-publisher={publisher}
+        data-metype-font-url={fontUrl || ""}
+        data-metype-font-family={fontFamily || ""}
+      ></div>
+    </div>
+  );
 };
 
 MetypeContributionWidget.propTypes = {
--- app/isomorphic/components/Metype/reaction-widget/index.js
+++ app/isomorphic/components/Metype/reaction-widget/index.js
@@ -1,39 +1,37 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { scriptLoader } from "../index";
 
 const MetypeReactionsWidget = (props) => {
   // eslint-disable-next-line react/prop-types
-  // const { accountId, host, storyUrl, storyId, fontUrl, fontFamily } = props;
-  // const [triggerInitPageReactions, setTriggerInitPageReactions] = useState(false);
-  // const reactionWrapper =
-  //   typeof document !== "undefined" && document.getElementById(`metype-page-reactions-container-${storyId}`);
-  // useEffect(() => {
-  //   !window.talktype && scriptLoader(host, () => initPageReactions(storyId));
-  //   if (triggerInitPageReactions) {
-  //     initPageReactions(storyId);
-  //   }
-  // }, [triggerInitPageReactions]);
-  // if (reactionWrapper && !triggerInitPageReactions) {
-  //   setTriggerInitPageReactions(true);
-  // }
-  // const initPageReactions = () => {
-  //   if (window.talktype && reactionWrapper) {
-  //     window.talktype.pageReactionsIframe(reactionWrapper);
-  //   }
-  // };
-  // return (
-  //   <div
-  //     style={{ marginTop: "20px" }}
-  //     id={`metype-page-reactions-container-${storyId}`}
-  //     data-metype-account-id={accountId}
-  //     data-metype-host={host}
-  //     data-metype-page-url={storyUrl}
-  //     data-metype-font-url={fontUrl || ""}
-  //     data-metype-font-family={fontFamily || ""}
-  //   ></div>
-  // );
-  return null;
+  const { accountId, host, storyUrl, storyId, fontUrl, fontFamily } = props;
+  const [triggerInitPageReactions, setTriggerInitPageReactions] = useState(false);
+  const reactionWrapper =
+    typeof document !== "undefined" && document.getElementById(`metype-page-reactions-container-${storyId}`);
+  useEffect(() => {
+    !window.talktype && scriptLoader(host, () => initPageReactions(storyId));
+    if (triggerInitPageReactions) {
+      initPageReactions(storyId);
+    }
+  }, [triggerInitPageReactions]);
+  if (reactionWrapper && !triggerInitPageReactions) {
+    setTriggerInitPageReactions(true);
+  }
+  const initPageReactions = () => {
+    if (window.talktype && reactionWrapper) {
+      window.talktype.pageReactionsIframe(reactionWrapper);
+    }
+  };
+  return (
+    <div
+      style={{ marginTop: "20px" }}
+      id={`metype-page-reactions-container-${storyId}`}
+      data-metype-account-id={accountId}
+      data-metype-host={host}
+      data-metype-page-url={storyUrl}
+      data-metype-font-url={fontUrl || ""}
+      data-metype-font-family={fontFamily || ""}
+    ></div>
+  );
 };
 
 export { MetypeReactionsWidget };
