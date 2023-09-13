a43695cd105fd3e41831c6a5f4ece717eac96afc
Devasena Murugesan <devasena.murugesan@quintype.com>
2023-09-13 14:38:49 +0530

    update const

--- cypress/e2e/sanitytest/sanityUI_1.cy.js
+++ cypress/e2e/sanitytest/sanityUI_1.cy.js
@@ -10,11 +10,12 @@ describe('Text and CSS Properties Test', () => {
     // Check if the URL contains specific text
     cy.url().should('include', 'malibu-advanced');
 
-    // Find the element containing the text you want to validate
-    cy.get('h3').should('be.visible').should('contain.text', 'AutoTest - Text Story');
+    // Find the element containing the text you want to validate and click on ut
+
     //xpath = //h3[normalize-space()='AutoTest - Text Story'];
-    const h3tag_autotest_textstory = cy.xpath("//h3[normalize-space()='AutoTest - Text Story']").click();
-    cy.get(h3tag_autotest_textstory).should('be.visible').should('contain.text', 'AutoTest - Text Story');
+    const h3tag_autotest_textstory = "//h3[normalize-space()='AutoTest - Text Story']"
+    cy.xpath(h3tag_autotest_textstory).click();
+
     //   cy.get('div') // You can replace 'div' with the appropriate selector of the parent element
   // .contains('h3', 'AutoTest - Text Story')
   // .click();
@@ -24,21 +25,18 @@ describe('Text and CSS Properties Test', () => {
 
     const title_xpath = "//*[@id=\"container\"]/div/div/div[1]/div/div[2]/div[2]/div[1]/h1/bdi";
     cy.get(title_xpath).should('have.css', 'font-family').and('match', /Arial, sans-serif/);
+    const fontFamily_name = cy.get(title_xpath.fontFamily);
+    cy.wrap(title_xpath).invoke('css', 'font-family').should('match', /Arial, sans-serif/);
     // Get the computed CSS properties of the element
     //cy.get('h1').should('have.css', 'font-family').and('match', /Arial, sans-serif/);
-    const fontfamily =
+
 
     // cy.get('your-element-selector').invoke('css', 'font-family').then((fontFamily) => {
     //   // Log the actual value to the Cypress command log
     //   cy.log('Actual Font Family:', fontFamily);
 
-      // Use the variable in your assertions or other logic
-      cy.wrap(fontFamily).should('match', /Arial, sans-serif/);
-    });
-
-
-
-    console.log('Actual Font Family:', fontFamily);
+    // Use the variable in your assertions or other logic
+    console.log('Actual Font Family:', fontFamily_name);
     cy.get('h1').should('have.css', 'font-size').and('eq', '40px');
     cy.get('h1').should('have.css', 'font-weight').and('eq', 'bold');
 
