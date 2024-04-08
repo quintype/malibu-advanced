dd5507c0323b1e0add83e5cde166832a85b01da9
Veena Y <veenay@quintypes-MacBook-Pro.local>
2024-04-08 20:45:44 +0530

    fix. vernacular tag/collection/section slug issue

--- app/server/data-loaders/tag-page-data.js
+++ app/server/data-loaders/tag-page-data.js
@@ -4,7 +4,7 @@ import { storyToCacheKey } from "@quintype/framework/server/caching";
 import { TAG_PAGE_URL_PREFIX } from "../../isomorphic/constants";
 
 export function loadTagPageData(client, tagSlug, config) {
-  return Story.getStories(client, "top", { "tag-slugs": tagSlug, limit: "9" }).then(stories => {
+  return Story.getStories(client, "top", { "tag-slugs": decodeURIComponent(tagSlug), limit: "9" }).then((stories) => {
     return client.getTags(tagSlug).then(({ tags }) => {
       const tagName = get(tags, [0, "name"], tagSlug);
       const tagDescription = get(tags, [0, "meta-description"], tagSlug);
@@ -12,16 +12,16 @@ export function loadTagPageData(client, tagSlug, config) {
         slug: tagSlug,
         name: tagName,
         description: tagDescription,
-        path: `${TAG_PAGE_URL_PREFIX}${tagSlug}`
+        path: `${TAG_PAGE_URL_PREFIX}${tagSlug}`,
       };
 
       return {
-        stories: stories.map(story => {
+        stories: stories.map((story) => {
           return { type: "story", story: story.asJson() };
         }),
         title: tagName,
-        cacheKeys: stories.map(story => storyToCacheKey(config["publisher-id"], story)),
-        tag
+        cacheKeys: stories.map((story) => storyToCacheKey(config["publisher-id"], story)),
+        tag,
       };
     });
   });
