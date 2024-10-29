cb5b9b07435b78526125d2d6611ddb3fc40b2090
Reena <reena@quintype.com>
2024-10-29 14:56:26 +0530

    support collection & home preview page

--- app/server/data-loaders/collection-page-data.js
+++ app/server/data-loaders/collection-page-data.js
@@ -1,10 +1,14 @@
-import { Collection } from "@quintype/framework/server/api-client";
+import { Collection } from '@quintype/framework/server/api-client'
 
-export function loadCollectionPageData(client, collectionSlug, config) {
-  return Collection.getCollectionBySlug(client, collectionSlug, { limit: 9 }, { depth: 2 }).then(collection => {
-    return {
-      collection: (collection && collection.asJson()) || {},
-      cacheKeys: collection && collection.cacheKeys(config["publisher-id"])
-    };
-  });
+export function loadCollectionPageData (client, params = {}, config, qtInternalAppsKey = '') {
+  const collectionSlug = params?.collectionSlug || ''
+  const opts = qtInternalAppsKey && params?.previewId ? { qtInternalAppsKey, previewId: params.previewId } : {}
+  return Collection.getCollectionBySlug(client, collectionSlug, { limit: 9 }, { depth: 2, ...opts }).then(
+    collection => {
+      return {
+        collection: (collection && collection.asJson()) || {},
+        cacheKeys: collection && collection.cacheKeys(config['publisher-id'])
+      }
+    }
+  )
 }
--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -1,21 +1,24 @@
 /* eslint-disable no-unused-vars */
-import { Collection } from "@quintype/framework/server/api-client";
-import { getStoryLimits, getNestedCollectionLimit } from "../../isomorphic/components/get-collection-template";
+import { Collection } from '@quintype/framework/server/api-client'
+import { getStoryLimits, getNestedCollectionLimit } from '../../isomorphic/components/get-collection-template'
 
-export async function loadHomePageData(client, config, slug = "home") {
+export async function loadHomePageData (client, config, params = {}, qtInternalAppsKey = '') {
+  const slug = params?.collectionSlug || 'home'
+  const opts = qtInternalAppsKey && params?.previewId ? { qtInternalAppsKey, previewId: params.previewId } : {}
   const collection = await Collection.getCollectionBySlug(
     client,
     slug,
-    { "item-type": "collection" },
+    { 'item-type': 'collection' },
     {
       depth: 2,
       storyLimits: getStoryLimits(),
       nestedCollectionLimit: getNestedCollectionLimit(),
-      defaultNestedLimit: 4
+      defaultNestedLimit: 4,
+      ...opts
     }
-  );
+  )
   return {
     collection: collection.asJson(),
-    cacheKeys: collection.cacheKeys(config["publisher-id"])
-  };
+    cacheKeys: collection.cacheKeys(config['publisher-id'])
+  }
 }
