ea843f254eb96bde6e0f794c88650b27f251763d
Nandakishore Prakash Rao <nandakishoreprakashrao@Nandakishores-MacBook-Pro.local>
2024-12-06 14:39:54 +0530

    Node-Upgrade: Fix npm install peer deps

--- app/isomorphic/components/form/index.js
+++ app/isomorphic/components/form/index.js
@@ -5,11 +5,11 @@ const Form = ({ formioUrl, disabled }) => {
   const [Formio, setFormComponent] = useState({});
   useEffect(() => {
     // SSR is not supported by react-formio
-    import(/* webpackChunkName: "qtc-react-formio" */ "react-formio")
-      .then(Formio => {
+    import(/* webpackChunkName: "qtc-react-formio" */ "@formio/react")
+      .then((Formio) => {
         setFormComponent({ component: Formio.Form });
       })
-      .catch(error => console.error("Error loading formio component", error));
+      .catch((error) => console.error("Error loading formio component", error));
   }, []);
 
   return (
