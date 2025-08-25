6fd8c399c3f205bde0dc3aeffbf3a3ca314b98a5
nandap4790 <nandap4790@gmail.com>
2025-08-25 13:24:37 +0530

    PR changes

--- webpack.config.js
+++ webpack.config.js
@@ -34,4 +34,17 @@ module.exports = {
   ...webpackConfig,
   module: { ...webpackModule, ...{ rules: enhancedRules } },
   plugins: enhancedPlugins,
+  // Add explicit dev server configuration
+  devServer: {
+    ...(webpackConfig.devServer || {}),
+    port: 8081,
+    host: '0.0.0.0',
+    allowedHosts: 'all',
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
 };
